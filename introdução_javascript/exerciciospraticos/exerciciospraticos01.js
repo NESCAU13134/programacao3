dc9043a7ebb763e147c0924d7b6f6272e0533aae
Luane Rafaela <153196900+NESCAU13134@users.noreply.github.com>
2024-12-02 15:10:46 -0300

    .
    .

---
+++ introdução_javascript/exerciciospraticos/exerciciospraticos01.js
@@ -0,0 +1,13 @@
+const {log} = require("Console");
+
+/* Crie um programa que declara variáveis usando var, let, e const, sendo: var do tipo
+String, let do tipo inteiro - Number, const do tipo float - Number. Imprima os valores
+das variáveis no terminal do Visual Studio Code*/
+
+var nome            = "Programação Estruturada";
+let idade           = 25;
+const ALIQUOTA_ICMS = 17.3;
+
+console.log("A variavel nome tem o valor: ", nome);
+console.log("A variavel idade tem o valor: ", idade);
+console.log("A variavel alicota_icms tem o valor: ", ALIQUOTA_ICMS);
---
+++ introdução_javascript/exerciciospraticos/exerciciospraticos02.js
@@ -0,0 +1,17 @@
+/*Questão 2
+Crie um programa que conte os números de 1 até 10, e os imprima na console.
+Utilize todas as estruturas de repetição disponíveis no JavaScript.*/
+
+for (let controleDoLacoFor = 1; controleDoLacoFor <= 10; controleDoLacoFor++){
+    console.log("Numero:", controleDoLacoFor);
+}
+console.log("/n");
+
+const NUMERO_MAXIMO_ITERACORES_LACO_WHILE = 10;
+let controleDoLacoWhile = 1;
+while (controleDoLacoWhile <= NUMERO_MAXIMO_ITERACORES_LACO_WHILE){
+    console.log("Laço de repetição While exibindo o número: ", controleDoLacoWhile);
+
+    controleDoLacoWhile++;
+}
+const arrayDeNumerosInteiros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
